e0d26c3297b2d35154314153a532e26b46e471fb
samansayad93 <sayadhelli10@gmail.com>
2025-03-06 17:59:08 +0330

    fix: refactor user model and repository for password handling and update JWT keys path

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"log"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/niflheimdevs/backend/internal/models"
 	"github.com/niflheimdevs/backend/internal/repositories"
 	"golang.org/x/crypto/bcrypt"
@@ -18,14 +21,20 @@ func NewUserService(userRepo repositories.UserRepo) *UserService {
 
 func (userService *UserService) AuthenticateUser(identifier string, password string) *models.UserModel {
 	user, err := userService.UserRepo.FindUserByUsername(identifier)
-	if err != nil {
+	if err == pgx.ErrNoRows {
 		user, err = userService.UserRepo.FindUserByEmail(identifier)
-		if err != nil {
-			panic(err)
+		if err == pgx.ErrNoRows {
+			//username/email not found
+			log.Print("username/email not found")
+			return &models.UserModel{}
 		}
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		panic(err)
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 
 	if err != nil {
 		panic(err)
