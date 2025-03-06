6997457e90c92b88aacf25538a6f2e56478d11dc
samansayad93 <sayadhelli10@gmail.com>
2025-03-06 13:45:16 +0330

    feat: add rate limiting middleware and update application structure

---
+++ internal/middlewares/ratelimit/ratelimit.go
@@ -0,0 +1,29 @@
+package middleware_rate_limit
+
+import (
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimit struct {
+	limit rate.Limit
+	burst int
+}
+
+func NewRateLimit() *RateLimit {
+	return &RateLimit{
+		limit: 5,
+		burst: 10,
+	}
+}
+
+func (rl *RateLimit) RateLimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := rate.NewLimiter(rl.limit, rl.burst)
+		if !limiter.Allow() {
+			panic("Rate Limit Exceed")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
