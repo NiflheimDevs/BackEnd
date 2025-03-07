673f47f6e86351e22b920f113fdcd67f21750305
kiasoh <kiarashs37@gmail.com>
2025-03-07 21:40:40 +0330

    update route

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -10,5 +10,8 @@ import (
 func Routes(app *wire.Application) http.Handler {
 	mux := chi.NewRouter()
 
+	mux.Post("/signup/otp", app.UserHandler.ReserveInfo)
+	mux.Post("/signup/verify", app.UserHandler.VerifyOTP)
+	mux.Get("/redis-test", app.UserHandler.RedisTest)
 	return mux
 }
