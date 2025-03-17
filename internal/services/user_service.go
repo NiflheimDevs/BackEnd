ad628ee121bc1449045fa279a5c2f54de05a3598
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2025-03-17 18:05:05 +0330

    fix: User (#19)

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -143,9 +143,9 @@ func (us *UserService) DeleteFromRedis(key string) {
 
 func (userService *UserService) AuthenticateUser(identifier string, password string) *models.UserModel {
 	user, err := userService.UserRepo.FindUserByUsername(identifier)
-	if err == nil {
+	if err != nil {
 		user, err = userService.UserRepo.FindUserByPhone(identifier)
-		if err == nil {
+		if err != nil {
 			panic(exceptions.Exception{
 				Tag: enums.NOT_FOUND,
 				Errors: []enums.SpecificError{
@@ -153,15 +153,6 @@ func (userService *UserService) AuthenticateUser(identifier string, password str
 				},
 			})
 		}
-	}
-
-	if err != nil {
-		panic(exceptions.Exception{
-			Tag: enums.NOT_FOUND,
-			Errors: []enums.SpecificError{
-				enums.USERNAME_PASSWORD_WRONG,
-			},
-		})
 	}
 
 	err = bcrypt.CompareHashAndPassword(user.Password, []byte(password))
@@ -179,6 +170,15 @@ func (userService *UserService) AuthenticateUser(identifier string, password str
 }
 
 func (userService *UserService) ChangePasswordValidate(user_id int, old_password string) {
+	if user_id == -1 {
+		panic(exceptions.Exception{
+			Tag: enums.AUTHENTICATION_ERROR,
+			Errors: []enums.SpecificError{
+				enums.AUTH_INVALID_CREDENTIALS,
+			},
+		})
+	}
+
 	user, err := userService.UserRepo.FindUserByID(user_id)
 
 	if err != nil {
