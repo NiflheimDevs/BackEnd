93612d00705ac026efe74c235febb581256b5638
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2025-03-17 18:05:05 +0330

    fix: User (#19)

--- internal/enums/error_tag.go
+++ internal/enums/error_tag.go
@@ -46,10 +46,11 @@ const (
 )
 
 const (
-	VALIDATION_ERROR GeneralError = "VALIDATION_ERROR"
-	UNAUTHORIZED     GeneralError = "UNAUTHORIZED"
-	INTERNAL_ERROR   GeneralError = "INTERNAL_ERROR"
-	NOT_FOUND        GeneralError = "NOT_FOUND"
-	BAD_REQUEST      GeneralError = "BAD_REQUEST"
-	LIMIT_EXCEED     GeneralError = "LIMIT_EXCEED"
+	VALIDATION_ERROR     GeneralError = "VALIDATION_ERROR"
+	UNAUTHORIZED         GeneralError = "UNAUTHORIZED"
+	INTERNAL_ERROR       GeneralError = "INTERNAL_ERROR"
+	NOT_FOUND            GeneralError = "NOT_FOUND"
+	BAD_REQUEST          GeneralError = "BAD_REQUEST"
+	LIMIT_EXCEED         GeneralError = "LIMIT_EXCEED"
+	AUTHENTICATION_ERROR GeneralError = "AUTHENTICATION_ERROR"
 )
