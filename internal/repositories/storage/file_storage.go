6e333ca83a5be17df669987c98fb32baf244373d
Kiasoh <kiarashs37@gmail.com>
2025-03-19 22:40:33 +0330

    add func to file storage

--- internal/repositories/storage/file_storage.go
+++ internal/repositories/storage/file_storage.go
@@ -1,8 +1,31 @@
 package storage
 
+import (
+	"os"
+
+	"github.com/niflheimdevs/backend/internal/exceptions"
+)
+
 type FileStorage struct {
 }
 
 func NewFileStorage() *FileStorage {
 	return &FileStorage{}
+}
+
+func (fs *FileStorage) SotorageFile(data []byte, outputName string) error {
+	outputFile, err := os.Create(outputName)
+	if err != nil {
+		panic(exceptions.Exception{
+			Tag: enums.SERVER_ERROR,
+		})
+	}
+	defer outputFile.Close()
+
+	_, err = outputFile.Write(outputBuffer.Bytes())
+	if err != nil {
+		panic(exceptions.Exception{
+			Tag: enums.SERVER_ERROR,
+		})
+	}
 }
