dd284a042a9e3dce87433892ba8846dc3a94006a
kiasoh <kiarashs37@gmail.com>
2025-03-23 03:36:00 +0330

    add constraint

--- migrations/CreateTableProjectTag.sql
+++ migrations/CreateTableProjectTag.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS "project_tag" (
   "project_id" int NOT NULL,
   "tag_id" int NOT NULL,
   FOREIGN KEY ("tag_id") REFERENCES "tag" ("id"),
-  FOREIGN KEY ("project_id") REFERENCES "project" ("id")
+  FOREIGN KEY ("project_id") REFERENCES "project" ("id"),
+  UNIQUE ("tag_id", "project_id")
 );
--- migrations/init.sql
+++ migrations/init.sql
@@ -142,7 +142,8 @@ CREATE TABLE IF NOT EXISTS "project_tag" (
   "project_id" int NOT NULL,
   "tag_id" int NOT NULL,
   FOREIGN KEY ("tag_id") REFERENCES "tag" ("id"),
-  FOREIGN KEY ("project_id") REFERENCES "project" ("id")
+  FOREIGN KEY ("project_id") REFERENCES "project" ("id"),
+  UNIQUE ("tag_id", "project_id")
 );
 
 CREATE TABLE IF NOT EXISTS "users_career_tag" (
