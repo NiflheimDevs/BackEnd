dc7a80348016ecbf4e1a4c654f9a23e29ccddd17
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2025-04-07 16:14:32 +0330

    Fix sprint 3 (#42)
    * fix: delete verification check for email update
    
    * fix: change verified email default value

--- migrations/init.sql
+++ migrations/init.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "users" (
   "username" varchar NOT NULL UNIQUE,
   "password" bytea NOT NULL,
   "email" varchar,
-  "is_verified" bool DEFAULT TRUE,
+  "is_verified" bool DEFAULT FALSE,
   "bio" text,
   "phone" varchar NOT NULL,
   "wallet" numeric DEFAULT 0,
