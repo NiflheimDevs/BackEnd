21a477187f2f2b194a0efb11ff3036762576329b
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2025-04-19 16:27:26 +0330

    feat: add landing project detail (#48)

--- internal/application/services/project_service.go
+++ internal/application/services/project_service.go
@@ -1,10 +1,12 @@
 package services
 
 import (
+	"github.com/niflheimdevs/backend/internal/application/dto"
 	"github.com/niflheimdevs/backend/internal/domain/models"
 )
 
 type ProjectService interface {
+	LandingProps() []dto.ProjectLanding
 	GetProject(projectID int) *models.ProjectModel
 	GetUserProjects(userID, offset, limit int) []models.ProjectModel
 	CreateProject(userID int, title, description string, label int, price int64, tags []int) int
