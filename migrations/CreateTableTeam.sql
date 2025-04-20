9161da6d29b4d69a7c828a63d3b450bd4b77b796
kiasoh <kiarashs37@gmail.com>
2025-04-20 21:13:13 +0330

    add created date to team table

--- migrations/CreateTableTeam.sql
+++ migrations/CreateTableTeam.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS "team" (
   "id" serial PRIMARY KEY,
   "type" int DEFAULT 0,
   "title" varchar,
-  "description" text
+  "description" text,
+  "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
