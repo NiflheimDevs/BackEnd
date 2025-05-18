e88d8b6f0e08eee9f0f030ff310d0a758795acd6
kiasoh <kiarashs37@gmail.com>
2025-05-18 15:11:29 +0330

    minor bug fixed

--- internal/application/services/impl/team_service_impl.go
+++ internal/application/services/impl/team_service_impl.go
@@ -454,7 +454,7 @@ func (ts *TeamService) UpdateMemeberRole(commanderid int, info *dto.UpdateMember
 			Tag: exceptions.CONFLICT_ERROR,
 		})
 	}
-	if member.Role.DoesHavePowerOver(targetMember.Role) {
+	if !member.Role.DoesHavePowerOver(targetMember.Role) {
 		log.Println("RoleError: user", member.Info.Userid, "doesn't have power over user", targetMember.Info.Userid, "in team", info.TeamID)
 		panic(exceptions.Exception{
 			Tag: exceptions.FORBIDDEN,
@@ -502,7 +502,7 @@ func (ts *TeamService) UpdatePosition(commanderid int, req *dto.UpdateMemberPosi
 			Tag: exceptions.CONFLICT_ERROR,
 		})
 	}
-	if member.Role.DoesHavePowerOver(targetMember.Role) {
+	if !member.Role.DoesHavePowerOver(targetMember.Role) {
 		panic(exceptions.Exception{
 			Tag: exceptions.FORBIDDEN,
 		})
