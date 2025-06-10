7f500a016d643f73197f4a38a68404b7be7408f2
kiasoh <kiarashs37@gmail.com>
2025-06-11 02:32:54 +0330

    change setup-elastic: use absolute path

--- scripts/setup-elasticsearch.sh
+++ scripts/setup-elasticsearch.sh
@@ -5,8 +5,8 @@ until curl -s http://elasticsearch:9200; do
   sleep 5
 done
 
-curl -X PUT http://elasticsearch:9200/users -H 'Content-Type: application/json' -d @create_index_users.json
-curl -X PUT http://elasticsearch:9200/teams -H 'Content-Type: application/json' -d @create_index_teams.json
-curl -X PUT http://elasticsearch:9200/projects -H 'Content-Type: application/json' -d @create_index_projects.json
+curl -X PUT http://elasticsearch:9200/users -H 'Content-Type: application/json' -d @/create_index_users.json
+curl -X PUT http://elasticsearch:9200/teams -H 'Content-Type: application/json' -d @/create_index_teams.json
+curl -X PUT http://elasticsearch:9200/projects -H 'Content-Type: application/json' -d @/create_index_projects.json
 
 echo "done"
