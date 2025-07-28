8378c5a83fc65646f8065d5c85ec6373d58abcf0
kiarash <kiarashs37@gmail.com>
2025-07-28 16:50:56 +0330

    add table to init.sql

--- migrations/init.sql
+++ migrations/init.sql
@@ -209,6 +209,22 @@ CREATE TABLE IF NOT EXISTS "users_team" (
   FOREIGN KEY ("role_id") REFERENCES "role" ("id")
 );
 
+CREATE TABLE IF NOT EXISTS "url_token" (
+  id              SERIAL PRIMARY KEY,
+  token           VARCHAR UNIQUE NOT NULL, 
+  purpose         VARCHAR NOT NULL,        
+  user_id         int,                   
+  invited_email   VARCHAR,    -- for sending mail to none website users            
+  team_id         int,                      
+  sender_id       int,                     
+  expires_at      TIMESTAMP WITH TIME ZONE,
+  created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+  FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL,
+  FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE,
+  FOREIGN KEY ("sender_id") REFERENCES "users"("id") ON DELETE SET NULL
+);
+
 DO $$
 BEGIN
   IF current_setting('app.skip_seeds', true) IS DISTINCT FROM 'on' THEN
