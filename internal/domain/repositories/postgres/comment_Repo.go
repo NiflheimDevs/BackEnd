083fb8facf632aec1b50d5623abf6af76b24f879
samansayad93 <sayadhelli10@gmail.com>
2025-07-31 20:18:43 +0330

    change some APIs

--- internal/domain/repositories/postgres/comment_Repo.go
+++ internal/domain/repositories/postgres/comment_Repo.go
@@ -8,6 +8,7 @@ import (
 type CommentRepo interface {
 	AddComment(projectID, bidID int, content string, star int) int
 	GetStar(userID int) (float64, error)
+	GetCommentCount(userID int) (int, error)
 	GetUserComments(userID int) ([]dto.CommentWithUserDTO, error)
 	GetCommentInfo(id int) dto.CommentWithUserDTO
 	GetCommentOfProject(projectID int) (*models.CommentModel, error)
