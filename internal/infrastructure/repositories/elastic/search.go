0c9c9598d55dcd61c26d1d5d7d9b317504e19df8
kiarash <kiarashs37@gmail.com>
2025-08-01 14:24:16 +0330

    idk testing

--- internal/infrastructure/repositories/elastic/search.go
+++ internal/infrastructure/repositories/elastic/search.go
@@ -430,7 +430,7 @@ func (se *SearchRepo) extractInnerHits(respond *elasticmodel.SearchResult) []map
 
 	var result []map[string]any
 
-	for i := 0; i < respond.Hits.Total.Value; i++ {
+	for i := 0; i < respond.Hits.Total.Value-1; i++ {
 		result = append(result, respond.Hits.Hits[i].Source)
 	}
 
